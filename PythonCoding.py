893db63472cc0633d3c2f01f6eb3c08348a8ec86
M.Santos <miguel.santos.student@gmail.com>
2020-11-18 13:17:51 -0500

    counting chars

--- PythonCoding.py
+++ PythonCoding.py
@@ -2,23 +2,20 @@ from pathlib import Path
 
 fpath = 'C:/Users/Miguel S/Downloads'
 data_folder = Path(fpath)
-input_file = data_folder / "rosalind_ini6.txt"
+input_file = data_folder / "rosalind_dna.txt"
 output_file = data_folder / "ouput.txt"
 reader = open(input_file, 'r')
 writer = open(output_file, 'w')
 
-words = reader.read().split()
-word_count = {}
+words = reader.read().strip()
+letter_count = {}
 for word in words:
-    # get the current word key and set its value to 
-    # its current value, zero if the word is not in the dictionary, 
-    # and add 1
-    word_count[word] = word_count.get(word,0) + 1
+    letter_count[word] = letter_count.get(word,0) + 1
 
 reader.close()
 
-for w in word_count:
-    writer.write(w + " " + str(word_count[w]) + '\n')
+for w in sorted(letter_count):
+    writer.write(str(letter_count[w]) + " ")
 
 writer.close()
 
